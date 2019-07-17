80a547abf4c5c91a5aaf1d1c42661d590687df1d
popcorn9499 <popcorn9499@gmail.com>
2019-07-17 15:07:39 -0500

    moving to async

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 from modules import deluge
+import asyncio
 
-delugeObj = deluge.deluge()
-
-
-version_number = delugeObj.send_request('webapi.get_api_version')
-assert version_number
-
-print('WebAPI version: %s' % version_number)
+async def test():
+    delugeObj = deluge.deluge()
 
 
-print(delugeObj.send_request('web.update_ui', [["label"], {"label":"tv-sonarr"}]))
-#fileSave("torrentsList.txt",delugeObj.send_request('web.update_ui', [["label"], {}]))
+    version_number = await delugeObj.send_request_async('webapi.get_api_version')
+    assert version_number
+
+    print('WebAPI version: %s' % version_number)
 
 
-#[["tracker", "label"], {}]
-
-version_number = delugeObj.send_request('webapi.get_torrents')
-assert version_number
-
-#print('WebAPI version: %s' % version_number)
+    print(await delugeObj.send_request_async('web.update_ui', [["label"], {"label":"tv-sonarr"}]))
+    #fileSave("torrentsList.txt",delugeObj.send_request('web.update_ui', [["label"], {}]))
 
 
+    #[["tracker", "label"], {}]
 
-print('Success')
+    version_number = await delugeObj.send_request_async('webapi.get_torrents')
+    assert version_number
+
+    #print('WebAPI version: %s' % version_number)
+
+
+
+    print('Success')
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test())
--- modules/deluge.py
+++ modules/deluge.py
@@ -1,6 +1,7 @@
 import requests
 from os import environ
 import json
+import asyncio
 
 class deluge():
     def __init__(self):
@@ -11,9 +12,36 @@ class deluge():
 
         assert self.send_request('auth.login', [self.password]), 'Unable to log in. Check password.'
 
-
-
     def send_request(self,method, params=None):
+
+        self.requestId += 1
+        try:
+            response = requests.post(
+                self.ipAddress,
+                json={'id': self.requestId, 'method': method, 'params': params or []},
+                cookies=self.cookies)
+
+        except requests.exceptions.ConnectionError:
+            raise Exception('WebUI seems to be unavailable. Run deluge-web or enable WebUI plugin using other thin client.')
+
+        data = response.json()
+
+        error = data.get('error')
+
+        if error:
+            print(error)
+            msg = error['message']
+
+            if msg == 'Unknown method':
+                msg += '. Check WebAPI is enabled.'
+
+            raise Exception('API response: %s' % msg)
+
+        self.cookies = response.cookies
+
+        return data['result']
+
+    async def send_request_async(self,method, params=None):
 
         self.requestId += 1
         try:
