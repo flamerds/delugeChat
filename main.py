55559e98e787c3d0f1c34a353db74bef97536f8f
popcorn9499 <popcorn9499@gmail.com>
2019-07-26 10:18:43 -0500

    add to main

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ async def test():
 
     #version_number = await delugeObj.send_request_async('webapi.get_api_version')
 
-    version_number = await delugeObj.send_request_async('web.get_hosts')
+    version_number = await delugeObj._send_request_async('web.get_hosts')
     assert version_number
 
     print('WebAPI version: %s' % version_number)
 
     hostId = version_number[0][0]
 
-    version_number = await delugeObj.send_request_async('web.connect',[hostId])
+    version_number = await delugeObj._send_request_async('web.connect',[hostId])
 
     await asyncio.sleep(10)
-    version_number = await delugeObj.send_request_async('web.connected')
+    version_number = await delugeObj._send_request_async('web.connected')
     assert version_number
 
     print('WebAPI version: %s' % version_number)
@@ -34,6 +34,10 @@ async def test():
 
     # 
 
+    await delugeObj.send_torrents("archlinux.torrent")
+
+
+    await delugeObj.send_torrents("magnet:?xt=urn:btih:d344c074850d158f501b012a9018e98fe97a23be&dn=archlinux-2019.07.01-x86_64.iso&tr=udp://tracker.archlinux.org:6969&tr=http://tracker.archlinux.org:6969/announce")
    
 
 
